4253a1056d5bfff52aff979c965c7e24253558ce
LondonChocoroll <jameswongwy@gmail.com>
2018-10-29 22:52:37 +1100

    Emit newmEssge on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,7 @@ var socket = io(); //critical for server requests
 socket.on('connect', function(){
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    from: 'Andrew',
-    text: 'yea that is ok'
-  });
+  
 });
 
 socket.on('disconnect', function(){
--- server/server.js
+++ server/server.js
@@ -15,15 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'sam@example.com',
-    text: 'Hey, what you up to',
-    createdAt: 123123
-  });
-
-
   socket.on('createMessage', (message)=>{
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', ()=>{
